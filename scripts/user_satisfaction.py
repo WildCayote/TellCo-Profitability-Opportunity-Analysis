1f15252b596da40b7e26d30872aa86ad1fc22a3a
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-09-10 06:30:45 +0300

    Feat: Finished writting the experience score function

--- scripts/user_satisfaction.py
+++ scripts/user_satisfaction.py
@@ -1,28 +1,46 @@
 import pandas as pd
+import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import Normalizer
 
 class UserStatisfactionCalculator:
 
     # columns used/aggregated to track the user experience
-    columns_of_experience = ["MSISDN/Number", "Avg RTT DL (ms)", "Avg RTT UL (ms)", "TCP DL Retrans. Vol (Bytes)", "TCP UL Retrans. Vol (Bytes)", "Handset Type", "Avg Bearer TP DL (kbps)", "Avg Bearer TP UL (kbps)"]
+    columns_of_experience = ["Avg RTT DL (ms)", "Avg RTT UL (ms)", "TCP DL Retrans. Vol (Bytes)", "TCP UL Retrans. Vol (Bytes)", "Handset Type", "Avg Bearer TP DL (kbps)", "Avg Bearer TP UL (kbps)"]
     
     # columns used/aggregated to track the user engagement
-    columns_of_engagement = ["MSISDN/Number", "Start", "Start ms", "End", "End ms", "Dur. (ms)", "Dur. (ms).1"]
+    columns_of_engagement = ["Start", "Start ms", "End", "End ms", "Dur. (ms)", "Dur. (ms).1"]
 
     def __init__(self, data: pd.DataFrame):
         self.data = data
-        self.engagement_clusters = None
-        self.experience_clusters = None
+        self.engagement_clusters_centers = None
+        self.experience_clusters_centers = None
         
+        self.normalizer = None
+
         self.customer_grouping = self.group_customers()
 
     def normalize_data(self, data: pd.DataFrame):
-        normalizer = Normalizer().fit(X=data)
+        if self.normalizer:
+            print('Using already initialized normalizer')
+            normalizer = self.normalizer
+        else: 
+            normalizer = Normalizer().fit(X=data)
+            print('Created a normalizer')
+
         normalized_data = normalizer.transform(X=data)
         normalized_data = pd.DataFrame(columns=data.columns, index=data.index, data=normalized_data)
         
         return normalized_data
+
+    def compute_euclidean_distance(self, center, data):
+        # Repeat center for each row of data
+        center_repeated = np.tile(center, (data.shape[0], 1))
+
+        # Compute Euclidean distances row-wise
+        distances = np.linalg.norm(data.values - center_repeated, axis=1)
+
+        return distances
 
     def group_customers(self):
         customer_grouping = self.data.groupby(by="MSISDN/Number")
@@ -59,7 +77,21 @@ class UserStatisfactionCalculator:
         ...
 
     def claculate_experience_score(self):
-        ...
+        # the worst experience center
+        worst_center = self.experience_cluster_centers[0]
+
+        ## now let us find the distance of users from this point
+        # aggregate the data
+        experience_agg = self.aggregate_experience_metrics()
+        experience_agg = experience_agg.drop(columns=["handset_type"])
+
+        # calculate the eucledean distance
+        result = self.compute_euclidean_distance(center=worst_center, data=experience_agg)
+
+        # add it to the respective columns
+        experience_agg['experience_score'] = result
+
+        return experience_agg
     
     def obtain_experience_cluster(self):
         # obtain the experience metrics for every user
