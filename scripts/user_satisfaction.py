6a268f090af3d45a532214f665a5dee3e0092a7f
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-09-10 06:52:19 +0300

    Feat: added a claculation for the engagement score

--- scripts/user_satisfaction.py
+++ scripts/user_satisfaction.py
@@ -73,12 +73,45 @@ class UserStatisfactionCalculator:
         
         return avg_stats
     
+    def aggregate_engagement_metrics(self):
+        customer_grouping = self.group_customers()
+        customer_data = customer_grouping.agg({
+            "Bearer Id": "count", # count the amount of session per sim card
+            "Dur. (ms)": "sum", # calculate the total duration per sim card in the data
+            "Total UL (Bytes)":  "sum", #calculate the total upload per sim card
+            "Total DL (Bytes)": "sum", # calculate the total download per sim card 
+        })
+
+        # finding the total trafic(sum between total download and uplaod)
+        customer_data["traffic"] = customer_data["Total UL (Bytes)"] + customer_data["Total DL (Bytes)"]
+
+        # renaming the fields
+        customer_data.rename(columns={
+            "Bearer Id": "session_freq",
+            "Dur. (ms)": "duration",
+            "Total UL (Bytes)": "upload_tot",
+            "Total DL (Bytes)": "download_tot"
+        }, inplace=True)
+
+        return customer_data
+
     def calculate_engagement_score(self):
-        ...
+        # the best experience center
+        best_center = self.engagement_clusters_centers[0]
+
+        ## now let us find the distance of users from this point
+        # aggrefate the data
+        engagement_agg = self.aggregate_engagement_metrics()
+        
+        # calculate the eucledean distance
+        result = self.compute_euclidean_distance(center=best_center, data=engagement_agg)
+
+        # add it to the respective rows
+        engagement_agg['engagement_score'] = result
 
     def claculate_experience_score(self):
         # the worst experience center
-        worst_center = self.experience_cluster_centers[0]
+        worst_center = self.experience_clusters_centers[0]
 
         ## now let us find the distance of users from this point
         # aggregate the data
@@ -88,7 +121,7 @@ class UserStatisfactionCalculator:
         # calculate the eucledean distance
         result = self.compute_euclidean_distance(center=worst_center, data=experience_agg)
 
-        # add it to the respective columns
+        # add it to the respective rows
         experience_agg['experience_score'] = result
 
         return experience_agg
@@ -110,12 +143,32 @@ class UserStatisfactionCalculator:
         experience_clusters = clusterer.fit(normalized_experience)
 
         # save the cluster centers
-        self.experience_cluster_centers = experience_clusters.cluster_centers_
+        self.experience_clusters_centers = experience_clusters.cluster_centers_
 
-        return self.experience_cluster_centers
+        return self.experience_clusters_centers
     
     def obtain_engagement_cluster(self):
-        ...
+        # obtain the engagement metrics for every user
+        engagement_agg = self.aggregate_engagement_metrics()
+
+        # drop the one categorical data
+        try:
+            engagement_agg = engagement_agg.drop(columns='handset_type')
+        except Exception as e:
+            pass
+        # normalize the data
+        normalized_engagement = self.normalize_data(data=engagement_agg)
+        
+        # initialize a clustering algorithm
+        clusterer = KMeans(n_clusters=3, init='k-means++', n_init=20, random_state=7)
+
+        # cluster the data
+        engagement_clusters = clusterer.fit(normalized_engagement)
+
+        # save the cluster centers
+        self.engagement_clusters_centers = engagement_clusters.cluster_centers_
+
+        return self.engagement_clusters_centers
     
     def obtain_satifisfaction_score(self):
         ...
