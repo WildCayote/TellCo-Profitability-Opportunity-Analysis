171367ed7295d01976331554237bf053fa30ceac
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-09-10 05:11:57 +0300

    Feat: Added a script that contains class for handling user satisfaction score, and implemented clustering

---
+++ scripts/user_satisfaction.py
@@ -0,0 +1,89 @@
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import Normalizer
+
+class UserStatisfactionCalculator:
+
+    # columns used/aggregated to track the user experience
+    columns_of_experience = ["MSISDN/Number", "Avg RTT DL (ms)", "Avg RTT UL (ms)", "TCP DL Retrans. Vol (Bytes)", "TCP UL Retrans. Vol (Bytes)", "Handset Type", "Avg Bearer TP DL (kbps)", "Avg Bearer TP UL (kbps)"]
+    
+    # columns used/aggregated to track the user engagement
+    columns_of_engagement = ["MSISDN/Number", "Start", "Start ms", "End", "End ms", "Dur. (ms)", "Dur. (ms).1"]
+
+    def __init__(self, data: pd.DataFrame):
+        self.data = data
+        self.engagement_clusters = None
+        self.experience_clusters = None
+        
+        self.customer_grouping = self.group_customers()
+
+    def normalize_data(self, data: pd.DataFrame):
+        normalizer = Normalizer().fit(X=data)
+        normalized_data = normalizer.transform(X=data)
+        normalized_data = pd.DataFrame(columns=data.columns, index=data.index, data=normalized_data)
+        
+        return normalized_data
+
+    def group_customers(self):
+        customer_grouping = self.data.groupby(by="MSISDN/Number")
+        return customer_grouping
+    
+    def aggregate_experience_metrics(self):
+        # aggregate the numeric values using average
+        avg_stats = self.customer_grouping.agg({
+            "Avg RTT DL (ms)": "mean",
+            "Avg RTT UL (ms)": "mean",
+            "TCP DL Retrans. Vol (Bytes)": "mean",
+            "TCP UL Retrans. Vol (Bytes)": "mean",
+            "Avg Bearer TP DL (kbps)": "mean",
+            "Avg Bearer TP UL (kbps)": "mean",
+            "Handset Type": lambda x: x.mode().iloc[0] # find the most frequent handset type for the user
+        })
+
+        # add the respective averages
+        avg_stats["avg_rtt"] = avg_stats[["Avg RTT DL (ms)" , "Avg RTT UL (ms)"]].mean(axis=1)
+        avg_stats["avg_tcp_rt"] = avg_stats[["TCP DL Retrans. Vol (Bytes)", "TCP UL Retrans. Vol (Bytes)"]].mean(axis=1)
+        avg_stats["avg_throughput"] = avg_stats[["Avg Bearer TP DL (kbps)", "Avg Bearer TP UL (kbps)"]].mean(axis=1)
+
+        # rename column Handset Type
+        avg_stats = avg_stats.rename(columns={
+            "Handset Type": "handset_type"
+        })
+
+        # drop the old columns
+        avg_stats = avg_stats.drop(columns=["Avg RTT DL (ms)" , "Avg RTT UL (ms)", "TCP DL Retrans. Vol (Bytes)", "TCP UL Retrans. Vol (Bytes)", "Avg Bearer TP DL (kbps)", "Avg Bearer TP UL (kbps)"])
+        
+        return avg_stats
+    
+    def calculate_engagement_score(self):
+        ...
+
+    def claculate_experience_score(self):
+        ...
+    
+    def obtain_experience_cluster(self):
+        # obtain the experience metrics for every user
+        experience_agg = self.aggregate_experience_metrics()
+
+        # drop the one categorical data
+        experience_agg = experience_agg.drop(columns='handset_type')
+
+        # normalize the data
+        normalized_experience = self.normalize_data(data=experience_agg)
+        
+        # initialize a clustering algorithm
+        clusterer = KMeans(n_clusters=3, init='k-means++', n_init=20, random_state=7)
+
+        # cluster the data
+        experience_clusters = clusterer.fit(normalized_experience)
+
+        # save the cluster centers
+        self.experience_cluster_centers = experience_clusters.cluster_centers_
+
+        return self.experience_cluster_centers
+    
+    def obtain_engagement_cluster(self):
+        ...
+    
+    def obtain_satifisfaction_score(self):
+        ...
